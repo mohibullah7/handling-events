f2ad5240a0a5dbb261dbb6507d0dbd3ef4f97fd6
Mohib khan mahsud <f20bseen1m01107@iub.edu.pk>
2024-08-30 00:04:53 +0500

    make chnges
    ok

--- handling-events/src/App.jsx
+++ handling-events/src/App.jsx
@@ -1,51 +1,25 @@
+import Card from "./Components/Card";
 
-import Card from './Components/Card'
-
-import './App.css'
-import { useState } from 'react'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  
-const  [score,setscore] = useState(true) ;
+  const [val1, valSet] = useState({
+    name:'mohib khan',
+     banned:10
+  });
 
-const [heading,chng]= useState('mohib khan mahsud')
-
-const Hdchng=()=>{
-  document.getElementById("hd").style.color='blue';
-  document.getElementById("hd").style.fontSize='40px';
-}
-
-const [val,valset] = useState(12)
   return (
     <>
-      {/* <Card/> */}
-      <h1>
-        
-        {score.toString()}
-
-      </h1>
-      <button onClick={()=>setscore(!score) }  className='py-3 px-4 m-5 bg-blue-700'>
-        click
-      </button>
-
-      <div className='flex items-center h-screen w-full bg-zinc-300 justify-center'>
-          <div className='w-80 bg-slate-100 p-5'>
-                <h1 id='hd' className='text-2xl bg-red-600'>
-                  {heading}
-                </h1>
-                <button  onClick={()=>chng('hoja chnage')}
-                 className='p-3 bg-blue-500 rounded-md'>change color</button>
-          </div>
-      </div>
-      <div className='flex flex-col  h-screen w-full bg-zinc-300 justify-center items-center'>
-          <h1>
-              {val}
-          </h1>
-          <button onClick={()=>valset((prechacha)=>(prechacha*3))}
-           className='p-3 bg-blue-500 rounded-full mt-10'>chnage number</button>
+      <div className="flex justify-center items-center flex-col bg-zinc-300">
+        <div className="bg-zinc-200 w-80 p-5">
+          <h1 className="text-3xl">{val1.name}</h1>
+          <h2 className="text-2xl">{val1.banned.toString()}</h2>
+          <button onClick={()=>valSet({...val1,banned: val1.banned})} className="p-3 bg-blue-900 rounded-md">change value</button>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
